24c9c6edfd24413fea7b937ff2efa4a329d3d0c2
Chau Le <nganchau.lenguyen@gmail.com>
2019-10-03 14:17:16 +0700

    Update bash script, organize utils

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,18 @@ pip install -r requirements.txt
 mkdir data/
 mkdir logs/
 
-FILE=/usr/local/bin/chromedriver_linux64
-if test -f "$FILE"; then
-    wget https://chromedriver.storage.googleapis.com/77.0.3865.40/chromedriver_linux64.zip
-    unzip -d /usr/local/bin $PWD/chromedriver_linux64.zip
-    rm $PWD/chromedriver_linux64.zip
+FILE=/usr/local/bin/chromedriver
+if ! test -f "$FILE"; then
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        LINK=https://chromedriver.storage.googleapis.com/77.0.3865.40/chromedriver_linux64.zip
+        FILE_NAME=chromedriver_linux64.zip
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        LINK=https://chromedriver.storage.googleapis.com/77.0.3865.40/chromedriver_mac64.zip
+        FILE_NAME=chromedriver_mac64.zip
+    fi
+    wget $LINK
+    unzip -d /usr/local/bin $PWD/$FILE_NAME
+    rm $PWD/$FILE_NAME
 fi
 
 python3 download_script.py
--- procedure/inference/predict.py
+++ procedure/inference/predict.py
@@ -1,63 +1,22 @@
 import logging
-from multiprocessing import Pool, cpu_count
-from functools import partial
 import numpy as np
 import torch
-from torch.utils.data import TensorDataset, DataLoader, SequentialSampler
-from keras.preprocessing.sequence import pad_sequences
-from pytorch_transformers import BertForSequenceClassification, BertTokenizer
-
-model = BertForSequenceClassification.from_pretrained(
-    "data/model/23919", num_labels=2)
-model.eval()
-
-tokenizer = BertTokenizer.from_pretrained(
-    "bert-base-uncased", do_lower_case=True)
+from .utils import prepare_input, get_model
 
 
+model = get_model()
 logger = logging.getLogger(__name__)
 mappings = {
     0: "NEGATIVE",
     1: "POSITIVE"
 }
-MAX_LEN = 50
-
-
-def tokenize(text):
-    text = '[CLS] ' + text + ' [SEP]'
-    tokens = tokenizer.tokenize(text)
-    input_ids = [tokenizer.convert_tokens_to_ids(token) for token in tokens]
-
-    return input_ids
-
-
-def get_attention_mask(sequence):
-    attention_mask = list(map(lambda x: float(x > 0), sequence))
-    return attention_mask
 
 
 def inference_batch(batch):
-    with Pool(cpu_count()) as p:
-        input_ids = list(p.map(tokenize, batch))
-
-        input_ids = pad_sequences(
-            input_ids, maxlen=MAX_LEN,
-            dtype="long", truncating="post", padding="post")
-
-        attention_mask = list(p.map(get_attention_mask, input_ids))
-
-    input_ids_list = torch.tensor(input_ids)
-    attention_mask_list = torch.tensor(attention_mask)
-
-    batch_size = 16
-    pr_data = TensorDataset(input_ids_list, attention_mask_list)
-    pr_sampler = SequentialSampler(pr_data)
-    pr_dataloader = DataLoader(
-        pr_data, sampler=pr_sampler, batch_size=batch_size)
-
+    dataloader = prepare_input(batch)
     res = []
 
-    for ids, mask in pr_dataloader:
+    for ids, mask in dataloader:
         with torch.no_grad():
             logits = model(ids, token_type_ids=None,
                            attention_mask=mask)[0].detach().numpy()
