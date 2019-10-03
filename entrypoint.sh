9880a948bbfa57e912be3c17458870566d15aa5b
Chau Le <nganchau.lenguyen@gmail.com>
2019-10-03 08:55:54 +0700

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,12 @@ pip install -r requirements.txt
 mkdir data/
 mkdir logs/
 
-wget https://chromedriver.storage.googleapis.com/77.0.3865.40/chromedriver_linux64.zip
-unzip -d /usr/local/bin $PWD/chromedriver_linux64.zip
-rm $PWD/chromedriver_linux64.zip
+FILE=/usr/local/bin/chromedriver_linux64
+if test -f "$FILE"; then
+    wget https://chromedriver.storage.googleapis.com/77.0.3865.40/chromedriver_linux64.zip
+    unzip -d /usr/local/bin $PWD/chromedriver_linux64.zip
+    rm $PWD/chromedriver_linux64.zip
+fi
 
 python3 download_script.py
 gunicorn --bind 0.0.0.0:5000 wsgi:app
