0b4fc4f8632cd33df734da7530bb24e5d7505a3f
Chau Le <nganchau.lenguyen@gmail.com>
2019-10-03 08:43:48 +0700

    Update code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+pip install -r requirements.txt
+
+mkdir data/
+mkdir logs/
+
 wget https://chromedriver.storage.googleapis.com/77.0.3865.40/chromedriver_linux64.zip
 unzip -d /usr/local/bin $PWD/chromedriver_linux64.zip
 rm $PWD/chromedriver_linux64.zip
+
 python3 download_script.py
 gunicorn --bind 0.0.0.0:5000 wsgi:app
