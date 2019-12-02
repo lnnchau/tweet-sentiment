f939d536855365ea72e7564bc1b1a983382d9372
Chau Le <nganchau.lenguyen@gmail.com>
2019-12-02 10:22:41 +0700

    Clean up

--- download_script.py
+++ download_script.py
@@ -8,47 +8,47 @@ gdd.download_file_from_google_drive(
     dest_path='data/model/distil.pkl')
 
 
-if not Path("data/model").exists():
-    processes = []
-    kwargs_list = [
-        {
-            "file_id": '1-J41m1vFJzmO32CPHShY9hk6b1NAvxNI',
-            "dest_path": 'data/model/23919/config.json'
-        },
-        {
-            "file_id": '11TmM1Bo-EHUflNWg83PDMLXcycmcIfTe',
-            "dest_path": 'data/model/28919/config.json'
-        }
-    ]
+# if not Path("data/model").exists():
+#     processes = []
+#     kwargs_list = [
+#         {
+#             "file_id": '1-J41m1vFJzmO32CPHShY9hk6b1NAvxNI',
+#             "dest_path": 'data/model/23919/config.json'
+#         },
+#         {
+#             "file_id": '11TmM1Bo-EHUflNWg83PDMLXcycmcIfTe',
+#             "dest_path": 'data/model/28919/config.json'
+#         }
+#     ]
 
-    for d in kwargs_list:
-        processes.append(
-            Process(target=gdd.download_file_from_google_drive, kwargs=d))
+#     for d in kwargs_list:
+#         processes.append(
+#             Process(target=gdd.download_file_from_google_drive, kwargs=d))
 
-    for p in processes:
-        p.start()
+#     for p in processes:
+#         p.start()
 
-    for p in processes:
-        p.join()
+#     for p in processes:
+#         p.join()
 
-    processes = []
-    kwargs_list = [
-        {
-            "file_id": '1-Ih2nLJ8S9a30AKne_447v9Sy2qIGcVJ',
-            "dest_path": 'data/model/23919/pytorch_model.bin'
-        },
-        {
-            "file_id": '11bi0zW7CnDCIAwPHO8S7fAGC-_J59ZJn',
-            "dest_path": 'data/model/28919/pytorch_model.bin'
-        }
-    ]
+#     processes = []
+#     kwargs_list = [
+#         {
+#             "file_id": '1-Ih2nLJ8S9a30AKne_447v9Sy2qIGcVJ',
+#             "dest_path": 'data/model/23919/pytorch_model.bin'
+#         },
+#         {
+#             "file_id": '11bi0zW7CnDCIAwPHO8S7fAGC-_J59ZJn',
+#             "dest_path": 'data/model/28919/pytorch_model.bin'
+#         }
+#     ]
 
-    for d in kwargs_list:
-        processes.append(
-            Process(target=gdd.download_file_from_google_drive, kwargs=d))
+#     for d in kwargs_list:
+#         processes.append(
+#             Process(target=gdd.download_file_from_google_drive, kwargs=d))
 
-    for p in processes:
-        p.start()
+#     for p in processes:
+#         p.start()
 
-    for p in processes:
-        p.join()
+#     for p in processes:
+#         p.join()
--- procedure/inference/predict.py
+++ procedure/inference/predict.py
@@ -2,7 +2,6 @@ import logging
 import numpy as np
 import torch
 import pickle
-from .utils import prepare_input, get_model
 
 
 model = pickle.load(open('data/model/distil.pkl', 'rb'))
@@ -23,19 +22,3 @@ def inference_batch(batch):
     labels = [mappings[r] for r in res]
     return labels, res
 
-# def inference_batch(batch):
-#     dataloader = prepare_input(batch)
-#     res = []
-
-#     for ids, mask in dataloader:
-#         with torch.no_grad():
-#             logits = model(ids, token_type_ids=None,
-#                            attention_mask=mask)[0].detach().numpy()
-
-#         res.extend(np.argmax(logits, axis=1).flatten())
-
-#         for tweet, label, l in list(zip(batch, res, logits.tolist())):
-#             logger.debug(f'\nL: {l}\nLabel: {label}\n{tweet}')
-
-#     labels = [mappings[r] for r in res]
-#     return labels, res
